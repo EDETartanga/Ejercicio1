6e9872d4b35fe5e54d02ba18964c96a84f22174d
Rosana <Rosana@DESKTOP-LI5B5FL>
2020-05-07 00:02:10 +0200

    rosana2

--- src/ejemplo1/App.java
+++ src/ejemplo1/App.java
@@ -5,7 +5,7 @@ public class App {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Ejercicio Ejemplo1");
-		//1.- Rosana
+		//1.- Rosana2
 	}
 
 }

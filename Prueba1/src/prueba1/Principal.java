2f71835a6bfc690f0848c8feadabc8248f2943b8
Rosana <Rosana@DESKTOP-LI5B5FL>
2021-05-10 00:09:50 +0200

    Commit 2

--- Prueba1/src/prueba1/Principal.java
+++ Prueba1/src/prueba1/Principal.java
@@ -4,7 +4,8 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hoal Mundo java-Git");
+		System.out.println("Hola Mundo java-Git");
+		System.out.println("subir a github");
 
 	}
 

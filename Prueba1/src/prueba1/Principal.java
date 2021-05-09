fcd2890553399faf7e935168cf94938a85455e39
Rosana <Rosana@DESKTOP-LI5B5FL>
2021-05-09 23:59:44 +0200

    Primer commit

---
+++ Prueba1/src/prueba1/Principal.java
@@ -0,0 +1,11 @@
+package prueba1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hoal Mundo java-Git");
+
+	}
+
+}
